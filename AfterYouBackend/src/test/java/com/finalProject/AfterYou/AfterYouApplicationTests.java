47755869a3db444a194e9aeee0fb7d7001f2c50d
L.Hiruna Pankaja <164976900+hirunaPankaja@users.noreply.github.com>
2025-04-21 11:39:51 +0530

    restructure file

---
+++ AfterYouBackend/src/test/java/com/finalProject/AfterYou/AfterYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalProject.AfterYou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
